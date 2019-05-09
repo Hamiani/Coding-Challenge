ecc45cfa3cb0727e7c546d27b634d459899d103c
hamiani <mohammed.hamiani@alphalupi.tech>
2019-05-09 13:20:41 +0000

    feat:scroll's edge

---
+++ src/Repositories-list/Repository.js
@@ -0,0 +1,35 @@
+import React from "react";
+const Repository = props => {
+  return (
+    <div>
+      {props.repositories.map((repo, index) => (
+        <div className="Repository" key={index}>
+          <div className="image">
+            <img alt="" height="150" width="150" src={repo.owner.avatar_url} />
+          </div>
+          <div className="details">
+            <div className="repoName"> {repo.name} </div>
+            <p className="repoDescription"> {repo.description} </p>
+            <div className="detailss">
+              <div className="stars">
+                Stars: {props.handleNumber(repo.stargazers_count)}
+              </div>
+              <div className="issues">
+                Issues: {props.handleNumber(repo.open_issues_count)}
+              </div>
+              <div className="submitted">
+                submitted
+                {" " +
+                  props.handleDaysDifference(repo.created_at.split("T")[0]) +
+                  " "}
+                days ago by {repo.owner.login}
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Repository;
--- src/Repositories-list/index.js
+++ src/Repositories-list/index.js
@@ -3,36 +3,35 @@ import axios from "axios";
 import InfiniteScroll from "react-infinite-scroller";
 import moment from "moment";
 import "../CSS/Css.css";
+import Repository from "./Repository";
 
 class RepositoryList extends Component {
   state = {
     repositories: [],
     per: 100,
-    page: 1
+    page: 1,
+    hasMore:true
   };
   loadRepositories = () => {
     const { per, page, repositories } = this.state;
     var date = new Date();
-    console.log(date);
     date.setDate(date.getDate() - 30);
     var diff = date.toISOString().split("T")[0];
-    console.log("///////////////", date.setDate(date.getDate() - 30));
-    console.log("*************diff", diff);
-    console.log(this.state.page);
-    console.log(this.state.per);
-
     const url = `https://api.github.com/search/repositories?q=created:>${diff}&sort=stars&order=desc&page=${page}&per_page=${per}`;
-    axios.get(url).then(
-      res => (
-        this.setState({
-          repositories: [...repositories, ...res.data.items],
-          page: page + 1,
-          per: per + 100
-        }),
-        console.log(this.state.repositories),
-        console.log(url)
-      )
+    axios.get(url).then(res =>
+      this.setState({
+        repositories: [...repositories, ...res.data.items],
+        page: page + 1
+      }),
+      console.log("page",this.state.page),
+      console.log("hasMore",this.state.hasMore)
     );
+    if(page>10){
+        this.setState({
+            hasMore:false
+        })
+        console.log("hasMore",this.state.hasMore)
+    }
   };
   handleNumber = num => {
     if (num >= 1000000000) {
@@ -64,45 +63,19 @@ class RepositoryList extends Component {
         Loading...
       </div>
     );
-    var Repositories = [];
-
-    this.state.repositories.map((repo, index) => {
-      Repositories.push(
-        <div className="Repository" key={index}>
-          <div className="image">
-            <img alt="" height="150" width="150" src={repo.owner.avatar_url} />
-          </div>
-          <div className="details">
-            <div className="repoName"> {repo.name} </div>
-            <p className="repoDescription"> {repo.description} </p>
-            <div className="detailss">
-              <div className="stars">
-                Stars: {this.handleNumber(repo.stargazers_count)}
-              </div>
-              <div className="issues">
-                Issues: {this.handleNumber(repo.open_issues_count)}
-              </div>
-              <div className="submitted">
-                submitted
-                {" " +
-                  this.handleDaysDifference(repo.created_at.split("T")[0]) +
-                  " "}
-                days ago by {repo.owner.login}
-              </div>
-            </div>
-          </div>
-        </div>
-      );
-    });
 
     return (
-      <InfiniteScroll 
+      <InfiniteScroll
         pageStart={this.state.page}
         loadMore={this.loadRepositories}
-        hasMore
+        hasMore={this.state.hasMore}
         loader={loader}
       >
-        {Repositories}
+        <Repository
+          repositories={this.state.repositories}
+          handleNumber={this.handleNumber}
+          handleDaysDifference={this.handleDaysDifference}
+        />
       </InfiniteScroll>
     );
   }
