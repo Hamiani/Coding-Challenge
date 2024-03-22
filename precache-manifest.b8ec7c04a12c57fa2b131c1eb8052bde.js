32866369cee74c770ae16bd6c48dec97edf2d138
Hamiani <hamiani.akbi@gmail.com>
2024-03-22 18:03:14 +0100

    Updates

---
+++ precache-manifest.b8ec7c04a12c57fa2b131c1eb8052bde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b3824da3151dc6c601a2068586d253",
+    "url": "/Coding-Challenge/index.html"
+  },
+  {
+    "revision": "3b70f932cd6471a1507b",
+    "url": "/Coding-Challenge/static/css/main.4fc3b58c.chunk.css"
+  },
+  {
+    "revision": "7204cfa28c650ca4ad56",
+    "url": "/Coding-Challenge/static/js/2.44c909f9.chunk.js"
+  },
+  {
+    "revision": "3b70f932cd6471a1507b",
+    "url": "/Coding-Challenge/static/js/main.26eff1ba.chunk.js"
+  },
+  {
+    "revision": "d74b12fb99f5948d1c83",
+    "url": "/Coding-Challenge/static/js/runtime~main.a8463884.js"
+  }
+]);
